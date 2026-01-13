30eb70bcbdeddf43a94085d75242d5651ed3aa3b
Iker Collada <ikercollada@gmail.com>
2026-01-13 18:29:08 +0100

    Update Tienda2026.java
    He añadido las cosas que estaban en el lapiz. Hay que hacer un push en clase.

--- tienda2026/src/main/java/cm69ikereducastur/com/tienda2026/Tienda2026.java
+++ tienda2026/src/main/java/cm69ikereducastur/com/tienda2026/Tienda2026.java
@@ -14,11 +14,34 @@ public class Tienda2026 {
         
     }
     
-    public static void validarDNI(String dni) {
+    public static boolean validarDNI(String dni) {
     //verificar que el DNI tiene un formato válido.
-        if (dni.isBlank() || !dni.matches("")) {
+        if (dni.isBlank() || !dni.matches("\\d(8)")) {
             return false;
         }
     }
+    
+    //Extraer el número y la letra del DNI.
+    String numero = dni.substring(0, 8);
+    char letra = dni.charAt(8);
+    
+    //Calcular la letra correspondiente al número del DNI
+    char letraCalculada = calcularLetraDNI(Integer.parseInt(numerosStr));
+    
+    /*Comparar la letra calculada con la letra proporcionada y devolver
+    el resultadode la comparación TRUE/FALSE. */
+    
+    return letra == letraCalculada;
+    
+    /* Devuelve TRUE si la letra del DNI y la calculada según la fórmula.
+    SE PUEDE DEVOLVER ASÍ EL RESULTADO DE UNA COMPARACIÓN. Se devuelve
+    TRUE si se cumple y FALSE sino. Es equivalente a poner:
+    
+    if 
+    
+    */
+    if (letra == letraCalculada) {
+        return TRUE;
+} //comentar la linea de codigo
     
 }
