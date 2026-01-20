4155213d1467d8b7fe02da539c16919ec182b1d1
1dawd23 <1dawd23@A204PC25.desarrolloweb.local>
2026-01-20 14:06:57 +0100

    Montando la tienda

---
+++ tienda2026/src/main/java/cm69ikereducastur/com/tienda2026/Cliente.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cm69ikereducastur.com.tienda2026;
+
+/**
+ *
+ * @author 1dawd23
+ */
+public class Cliente {
+    private String idCliente;
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Cliente(String idCliente, String nombre, String telefono, String email) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String IdCliente) {
+        this.idCliente = IdCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return idCliente  + " - " + nombre + " - " + telefono + " - " + email;
+    }
+}
--- tienda2026/src/main/java/cm69ikereducastur/com/tienda2026/comparable.java
+++ tienda2026/src/main/java/cm69ikereducastur/com/tienda2026/comparable.java
@@ -1,17 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-
 package cm69ikereducastur.com.tienda2026;
 
 /**
  *
  * @author 1dawd23
  */
-public class Tienda2026 {
-
-    public static void main(String[] args) {
-        
-    }
+interface comparable<T> {
     
 }
